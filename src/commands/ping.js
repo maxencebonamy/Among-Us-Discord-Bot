3282e271de46a1a38053178fbb99f66cba07ed3b
Maxence Bonamy <maxencebonamy@gmail.com>
2023-12-14 15:21:12 +0100

    Update .gitignore and .vscode settings, modify package.json and ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,9 +1,33 @@
-import { SlashCommandBuilder } from "discord.js"
+import { SlashCommandBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } from "discord"
 
 export const data = new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with Pong!")
 
-export async function execute(interaction) {
-    return interaction.reply("Pong!")
+export const execute = async(interaction) => {
+    const select = new StringSelectMenuBuilder()
+			.setCustomId('starter')
+			.setPlaceholder('Make a selection!')
+			.addOptions(
+				new StringSelectMenuOptionBuilder()
+					.setLabel('Bulbasaur')
+					.setDescription('The dual-type Grass/Poison Seed Pokémon.')
+					.setValue('bulbasaur'),
+				new StringSelectMenuOptionBuilder()
+					.setLabel('Charmander')
+					.setDescription('The Fire-type Lizard Pokémon.')
+					.setValue('charmander'),
+				new StringSelectMenuOptionBuilder()
+					.setLabel('Squirtle')
+					.setDescription('The Water-type Tiny Turtle Pokémon.')
+					.setValue('squirtle'),
+			);
+
+		const row = new ActionRowBuilder()
+			.addComponents(select);
+
+		await interaction.reply({
+			content: 'Choose your starter!',
+			components: [row],
+		});
 }
